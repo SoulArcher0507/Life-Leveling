b1ed199bff0277c39b8f3c4236304b1efc1f9f29
SoulArcher04 <enea.ares2004@gmail.com>
2025-03-21 18:51:38 +0100

    push

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ""),
     );
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+  
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -60,28 +61,85 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Mantieni l'AppBar
       appBar: AppBar(
         title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
       ),
-
-      body: Row(
-        children: [
-          MyNavigationBar(
-            onDestinationSelected: (int index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-),
-          Expanded(
-            child: IndexedStack(
-              index: _currentIndex,
-              children: _pages,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
             ),
-          ),
-        ],
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 0;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.task),
+              title: Text('Tasks'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 1;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.note),
+              title: Text('Memos'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 2;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.bar_chart),
+              title: Text('Graphs'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 3;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 4;
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
+      body: _pages[_currentIndex],
     );
   }
 }
--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,86 +1,56 @@
 import 'package:flutter/material.dart';
 
-class MyNavigationBar extends StatefulWidget {
-  final ValueChanged<int> onDestinationSelected;
+class NavigationDrawer extends StatelessWidget {
+  final Function(int) onItemSelected;
 
-  const MyNavigationBar({Key? key, required this.onDestinationSelected}) : super(key: key);
-
-  @override
-  State<MyNavigationBar> createState() => _MyNavigationBarState();
-}
-
-class _MyNavigationBarState extends State<MyNavigationBar> {
-  bool _isExpanded = false;
-  int _selectedIndex = 0;
+  const NavigationDrawer({Key? key, required this.onItemSelected}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          right: BorderSide(color: Colors.grey.shade300, width: 1), // Bordo destro
-        ),
-      ),
-      child: Column(
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          // Pulsante per espandere/ridurre la barra laterale con testo "Menù" a destra
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.menu),
-                  onPressed: () {
-                    setState(() {
-                      _isExpanded = !_isExpanded;
-                    });
-                  },
-                ),
-                if (_isExpanded)
-                  const Text(
-                    'Menù',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-              ],
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+            ),
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
             ),
           ),
-          // NavigationRail
-          Expanded(
-            child: NavigationRail(
-              extended: _isExpanded,
-              selectedIndex: _selectedIndex,
-              onDestinationSelected: (int index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-                widget.onDestinationSelected(index); // Notifica il cambio di indice
-              },
-              destinations: const <NavigationRailDestination>[
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.checklist),
-                  label: Text('Task'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.add_alert),
-                  label: Text('Promemoria'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.auto_graph),
-                  label: Text('Grafico'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.settings),
-                  label: Text('Impostazioni'),
-                ),
-              ],
-            ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () => onItemSelected(0),
+          ),
+          ListTile(
+            leading: Icon(Icons.task),
+            title: Text('Tasks'),
+            onTap: () => onItemSelected(1),
+          ),
+          ListTile(
+            leading: Icon(Icons.note),
+            title: Text('Memos'),
+            onTap: () => onItemSelected(2),
+          ),
+          ListTile(
+            leading: Icon(Icons.bar_chart),
+            title: Text('Graphs'),
+            onTap: () => onItemSelected(3),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => onItemSelected(4),
           ),
         ],
       ),
     );
   }
 }
+
